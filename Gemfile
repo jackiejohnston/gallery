9c46ca0a6236f5d8d599aa2ba381c16120f8078f
Jackie Johnston <jackie.johnston@pnmac.com>
2016-09-16 12:01:43 -0700

    Add Gemfile, initial config and js setup

---
+++ Gemfile
@@ -0,0 +1,9 @@
+ source 'https://rubygems.org'
+
+ group :jekyll_plugins do
+  gem 'jekyll'
+  gem 'jekyll-stitch-plus'
+  gem 'uglifier'
+  gem 'hawkins'
+  gem 'jekyll-autoprefixer'
+ end
---
+++ js/site.js
@@ -0,0 +1,29 @@
+$(window).load(function() {
+  // Initialize the Material Bootstrap Design javascript:
+  $.material.init()
+  // Initialize copy buttons:
+  new Clipboard('.copy-this-label');
+  // Initialize pop-overs & tooltips
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Add active class to menu links when you are on the page:
+  function stripTrailingSlash(str) {
+    if(str.substr(-1) == '/') {
+      return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+  var locationPage = stripTrailingSlash(window.location.pathname);
+  $.each($('header .nav').find('li'), function() {
+    if ($(this).find('a')) {   
+      var parser = document.createElement('a');
+      parser.href = $(this).find('a').attr('href');
+      if (stripTrailingSlash(parser.pathname) === locationPage) {
+        $(this).addClass('active');
+      }
+    }
+  });
+
+
+});
